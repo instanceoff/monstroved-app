c27588aea8320284331ba3970e20551956b3b07b
Ilya "instance" Khinevich <36165959+instanceoff@users.noreply.github.com>
2022-10-20 12:05:12 +0300

    Update firebaseCofig.ts

--- src/api/firebaseCofig.ts
+++ src/api/firebaseCofig.ts
@@ -8,12 +8,6 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAbrGDyqL7j8xP-MDu2CZ_24GgQUUjX9QM',
-  authDomain: 'monstrovedproject.firebaseapp.com',
-  projectId: 'monstrovedproject',
-  storageBucket: 'monstrovedproject.appspot.com',
-  messagingSenderId: '4323445550',
-  appId: '1:4323445550:web:731ff4db2a96695e4b08bf',
 };
 
 // Initialize Firebase
